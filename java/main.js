bf8cdd97d333d4521bcd09f41086a2b8342d21d1
murakore <77715166+murakore@users.noreply.github.com>
2022-02-03 17:37:57 +0900

    Update main.js

--- java/main.js
+++ java/main.js
@@ -2,46 +2,98 @@ const date = new Date();
 const month = date.getMonth() + 1;
 const hour = date.getHours();
 const b = document.getElementById("body");
-
-async function fetchImages(breed) {
-    const response = await fetch(
-      `https://dog.ceo/api/breed/${breed}/images/random/1`
-    );
-    const data = await response.json();
-}
-
+const div = document.querySelector('.localStorage');
+const btnSave = document.querySelector('.btnSave');
+const btnRead = document.querySelector('.btnRead');
+const input = document.querySelector('.input');
 
 
 if(month>=3 && month<=5) {
     if(hour<=8) {
-        b.style.backgroundImage = ' url("./images/data1.jpg")';
+        b.style.backgroundImage = ' url("https://images.dog.ceo/breeds/shiba/shiba-1.jpg")';
+        btnSave.addEventListener('click', () => {
+            const data = input.value;
+            localStorage.setItem('myKey1',data);
+        });
+        btnRead.addEventListener('click', () => {
+            const data = localStorage.getItem('mykey1');
+            input.value = data;
+        });
     }else if(hour<=16) {
-        b.style.backgroundImage = ' url("./images/data2.jpg")';
+        b.style.backgroundImage = ' url("https://images.dog.ceo/breeds/shiba/shiba-2.jpg")';
+        btnSave.addEventListener('click', () => {
+            const data = input.value;
+            localStorage.setItem('myKey2',data);
+        });
     }else if(hour<=24) {
-        b.style.backgroundImage = ' url("./images/data3.jpg")';
+        b.style.backgroundImage = ' url("https://images.dog.ceo/breeds/shiba/shiba-3.jpg")';
+        btnSave.addEventListener('click', () => {
+            const data = input.value;
+            localStorage.setItem('myKey3',data);
+        });
     }
 }else if(month>=6 && month<=8) {
     if(hour<=8) {
-        b.style.backgroundImage = ' url("./images/data4.jpg")';
+        b.style.backgroundImage = ' url("https://images.dog.ceo/breeds/shiba/shiba-4.jpg")';
+        btnSave.addEventListener('click', () => {
+            const data = input.value;
+            localStorage.setItem('myKey4',data);
+        });
     }else if(hour<=16) {
-        b.style.backgroundImage = ' url("./images/data5.jpg")';
+        b.style.backgroundImage = ' url("https://images.dog.ceo/breeds/shiba/shiba-5.jpg")';
+        btnSave.addEventListener('click', () => {
+            const data = input.value;
+            localStorage.setItem('myKey5',data);
+        });
     }else if(hour<=24) {
-        b.style.backgroundImage = ' url("./images/data6.jpg")';
+        b.style.backgroundImage = ' url("https://images.dog.ceo/breeds/shiba/shiba-6.jpg")';
+        btnSave.addEventListener('click', () => {
+            const data = input.value;
+            localStorage.setItem('myKey6',data);
+        });
     } 
 }else if(month>=9 && month<=11) {
     if(hour<=8) {
-        b.style.backgroundImage = ' url("./images/data7.jpg")';
+        b.style.backgroundImage = ' url("https://images.dog.ceo/breeds/shiba/shiba-7.jpg")';
+        btnSave.addEventListener('click', () => {
+            const data = input.value;
+            localStorage.setItem('myKey7',data);
+        });
     }else if(hour<=16) {
-        b.style.backgroundImage = ' url("./images/data8.jpg")';
+        b.style.backgroundImage = ' url("https://images.dog.ceo/breeds/shiba/shiba-8.jpg")';
+        btnSave.addEventListener('click', () => {
+            const data = input.value;
+            localStorage.setItem('myKey8',data);
+        });
     }else if(hour<=24) {
-        b.style.backgroundImage = ' url("./images/data9.jpg")';
+        b.style.backgroundImage = ' url("https://images.dog.ceo/breeds/shiba/shiba-9.jpg")';
+        btnSave.addEventListener('click', () => {
+            const data = input.value;
+            localStorage.setItem('myKey9',data);
+        });
     }
 }else if(month<=2 || month==12) {
     if(hour<=8) {
-        b.style.backgroundImage = ' url("https://dog.ceo/api/breed/shiba/images/random/1")';
-    }else if(hour<=16) {
-        b.style.backgroundImage = ' url("https://images.dog.ceo/breeds/shiba/shiba-9.jpg")';
-    }else if(hour<=24) {
         b.style.backgroundImage = ' url("https://images.dog.ceo/breeds/shiba/shiba-10.jpg")';
+        btnSave.addEventListener('click', () => {
+            const data = input.value;
+            localStorage.setItem('myKey10',data);
+        });
+    }else if(hour<=16) {
+        b.style.backgroundImage = ' url("https://images.dog.ceo/breeds/shiba/shiba-11.jpg")';
+        btnSave.addEventListener('click', () => {
+            const data = input.value;
+            localStorage.setItem('myKey11',data);
+        });
+    }else if(hour<=24) {
+        b.style.backgroundImage = ' url("https://images.dog.ceo/breeds/shiba/shiba-12.jpg")';
+        btnSave.addEventListener('click', () => {
+            const data = input.value;
+            localStorage.setItem('myKey12',data);
+        });
+        btnRead.addEventListener('click', () => {
+            const data = localStorage.getItem('mykey1');
+            input.value = data;
+        });
     }
 }
